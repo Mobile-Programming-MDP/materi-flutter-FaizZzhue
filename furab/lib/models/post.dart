e228147bd8b73afd02fdb2c0a1eb384fe93537d0
FaizZzhue <fynyourbaeid18@gmail.com>
2026-04-30 22:19:44 +0700

    chore: add logic and finisher

--- furab/lib/models/post.dart
+++ furab/lib/models/post.dart
@@ -2,34 +2,34 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Post {
   String? id;
-  String? imageBase64;
-  final String description;
-  final String category;
+  String? image;
+  String? description;
+  String? category;
   Timestamp? createdAt;
   Timestamp? updatedAt;
-  String? latitude;
-  String? longitude;
+  double? latitude;
+  double? longitude;
   String? userId;
   String? userFullName;
 
   Post({
     this.id,
-    this.imageBase64,
-    required this.description,
-    required this.category,
+    this.image,
+    this.description,
+    this.category,
     this.createdAt,
     this.updatedAt,
     this.latitude,
     this.longitude,
     this.userId,
-    this.userFullName
+    this.userFullName,
   });
 
   factory Post.fromDocument(DocumentSnapshot doc) {
     Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
     return Post(
       id: doc.id,
-      imageBase64: data['image_base_64'],
+      image: doc['image'],
       description: data['description'],
       category: data['category'],
       createdAt: data['created_at'] as Timestamp,
@@ -37,21 +37,21 @@ class Post {
       latitude: data['latitude'],
       longitude: data['longitude'],
       userId: data['user_id'],
-      userFullName: data['user_full_name']
+      userFullName: data['user_full_name'],
     );
   }
 
   Map<String, dynamic> toDocument() {
     return {
-      'image_base64': imageBase64,
+      'image': image,
       'description': description,
       'category': category,
-      'created_at': createdAt,
-      'updated_at': updatedAt,
       'latitude': latitude,
       'longitude': longitude,
+      'created_at': createdAt,
+      'updated_at': updatedAt,
       'user_id': userId,
-      'user_full_name': userFullName
+      'user_full_name': userFullName,
     };
   }
 }
